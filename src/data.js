6dc4dd5427c31be7a2d69a923482760a9ed1cdf9
Lesliee97 <eilsel1718rv@gmail.com>
2019-09-19 16:05:29 -0500

    repo de localStorage

--- src/data.js
+++ src/data.js
@@ -1,61 +1,23 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-// ORDENAR DE LA A-Z Z-A
-const ordenarPoke = (data) => {
-  return data.sort((objeto1, objeto2) => { // CALLBACKS ES UNA FUNCIÓN QUE SE PASA A OTRA FUNCIÓN COMO UN ARGUMENTO, QUE LUEGO SE INVOCA DENTRO DE LA FUNCIÓN EXTERNA PARA COMPLETAR ALGÚN TIPO DE RUTINA O ACCIÓN.
-    if (objeto1.name > objeto2.name) { // FUNCION COMPRATIVA (COMPARATIVE FUNCTION)ASCENDENTE 
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+const validarEmail = (correo) => {
+  const correoCorrecto = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  correoCorrecto.test(correo);
+};
+const savePost = (arr, ele) => {
+  arr.push(ele);
+  return arr;
+};
+const eliminarObj = (arr, indice) =>{
+  arr.splice(indice, 1);
+  return arr;
+};
+const editPost = (arr, indice, element) => {
+  const result = [...arr];
+  result[indice] = {...result[indice]};
+  result[indice].text = element[indice].value;
+  return result;
 };
 
-// ORDENAR SPAW
-const ordenarAsc = (data) => {
-  return data.sort((objeto1, objeto2) => objeto1.avg_spawns - objeto2.avg_spawns);
-};
-
-// TIPOS
-const funcionTipos = (data, tipos) => {
-  return data.filter((element) => {
-    return element.type.indexOf(tipos) > -1;
-  });
-  return pokeType; // mi valor va sea devuelto cuando llame a la funcion
-};
-
-// DEBILIDADES
-const funcionDebilidades = (data, debilidad) => {
-  return data.filter((elemento) => {
-    return elemento.weaknesses.indexOf(debilidad) > -1;
-    // Si la cadena no existe devuelve -1 
-    // > -1 evuelve el índice, dentro del objeto
-    // indeof me esta devolvoiendo el indice de la data que se esta recorriendo
-  });
-};
-
-// HUEVOS 
-const huevoEclo = (data, kilometro) => {
-  return data.filter((elemet) => {
-    return elemet.egg.indexOf(kilometro) > -1; // INDICE DE MI DATA QUE SE ESTA RECORRIENDO
-  });
-};
-
-// DATA BUSCAR
-const busco = (data, buscado) => {
-  return data.filter(obj => obj.name.toLowerCase().startsWith(buscado)); 
-  // Filter va a crear un array donde va rellenar todos los elementos que pasa un prueba
-};
-
-window.ordenarPoke = ordenarPoke;
-window.ordenarAsc = ordenarAsc;
-window.funcionTipos = funcionTipos;
-window.funcionDebilidades = funcionDebilidades;
-window.huevoEclo = huevoEclo;
-window.busco = busco;
-
-// change almacena un valor
-// imput captura un valor pero no lo almacena
+window.validarEmail = validarEmail;
+window.savePost = savePost;
+window.eliminarObj = eliminarObj;
+window.editPost = editPost;
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,115 +1,47 @@
 global.window = global;
-// global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
-const dataPoke = [
-  {
-    'id': 2,
-    'num': '002',
-    'name': 'Ivysaur',
-    'type': [
-      'Grass',
-      'Poison'
-    ],
-    'egg': 'Not in Eggs',
-    'avg_spawns': 4.2,
-    'weaknesses': [
-      'Fire',
-      'Ice',
-      'Flying',
-      'Psychic'
-    ]
-  },
-  {
-    'id': 4,
-    'num': '004',
-    'name': 'Charmander',
-    'type': [
-      'Fire'
-    ],
-    'egg': '2 km',
-    'avg_spawns': 25.3,
-    'weaknesses': [
-      'Water',
-      'Ground',
-      'Rock'
-    ],
-  }, {
-    'id': 1,
-    'num': '001',
-    'name': 'Bulbasaur',
-    'img': 'http://www.serebii.net/pokemongo/pokemon/001.png',
-    'type': [
-      'Grass',
-      'Poison'
-    ],
-    'egg': '2 km',
-    'avg_spawns': 69,
-    'weaknesses': [
-      'Fire',
-      'Ice',
-      'Flying',
-      'Psychic'
-    ]
-  }, {
-    'id': 3,
-    'num': '003',
-    'name': 'Venusaur',
-    'type': [
-      'Grass',
-      'Poison'
-    ],
-    'egg': 'Not in Eggs',
-    'avg_spawns': 1.7,
-    'weaknesses': [
-      'Fire',
-      'Ice',
-      'Flying',
-      'Psychic'
-    ],
-  },
+
+describe('validarEmail', () => {
+  it('debería ser una función', () => {
+    expect(typeof global.validarEmail).toEqual('function');
+  });
+});
+
+const arr = ['blanco', 'negro', 'rosado'];
+const arr2 = ['blanco', 'negro', 'rosado'];
+const arr3 = [
+  {text: 'blanco'}, 
+  {text: 'negro'}, 
+  {text: 'rosado'}
 ];
 
+describe('eliminarObj', () => {
+  it('debería ser una funcion', () => {
+    expect(typeof global.eliminarObj).toEqual('function');
+  });
+  it('deberia eliminar el elemento de un array', () => {
+    expect(global.eliminarObj(arr2, 2)).toEqual(['blanco', 'negro']);
+  });
+});
+const ele = 'morado';
+const ele2 = [{}, {value: 'morado'}];
 
-describe('busco', () => {
-  it('deberia retornar bulbasaur', () => {
-    expect(busco(dataPoke, 'bul')[0].name).toEqual('Bulbasaur');
+describe('savePost', () => {
+  it('debería ser una funcion', () => {
+    expect(typeof global.savePost).toEqual('function');
+  });
+  it('debería retornar un array', () => {
+    expect(savePost(arr, ele)).toEqual(['blanco', 'negro', 'rosado', 'morado']);
   });
 });
 
-describe('ordenarAsc', () => {
-  it('deberia retornar ascendente', () => {
-    expect(ordenarAsc(dataPoke, '2')[0].name).toEqual('Venusaur');
+describe('editPost', () => {
+  it('debería ser una funcion', () => {
+    expect(typeof global.editPost).toEqual('function');
   });
-});
-
-describe('ordenarPoke', () => {
-  it('deberia retornar AZ', () => {
-    expect(ordenarPoke(dataPoke, '0')[0].name).toEqual('Bulbasaur');
-  });
-});
-
-describe('ordenarPoke', () => {
-  it('deberia retornar ZA', () => {
-    expect(ordenarPoke(dataPoke, '1')[0].name).toEqual('Bulbasaur');
-  });
-});
-
-describe('funcionTipos', () => {
-  it('deberia mostrar los de tipos Grass', () => {
-    expect(funcionTipos(dataPoke, 'Grass')[0].type).toEqual(['Grass', 'Poison']);
-  });
-});
-
-describe('funcionDebilidades', () => {
-  it('deberia mostrar las debilidades Water', () => {
-    expect(funcionDebilidades(dataPoke, 'Water')[0].weaknesses).toEqual(['Water', 'Ground', 'Rock']);
-  });
-});
-
-describe('huevoEclo', () => {
-  it('deberia mostrar los pokemones que tiene huevos de 2km', () => {
-    expect(huevoEclo(dataPoke, '2 km')[0].egg).toEqual('2 km');
+  it('debería modificar un elemento array', () => {
+    expect(editPost(arr3, 1, ele2)).toEqual([{text: 'blanco'}, {text: 'morado'}, {text: 'rosado'}]);
   });
 });
